a0908709bd0cf7550de15241172165cb7bd78ade
felipeaziz <felipeaziz@gmail.com>
2026-01-21 14:07:21 +0000

    Update settings and URLs to support local and Codespace environments

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import api, views
+from django.http import JsonResponse
+import os
 
 router = DefaultRouter()
 router.register(r'users', api.UserViewSet, basename='user')
@@ -29,4 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('', views.api_root, name='api-root'),
+    path('api/root_url/', lambda request: JsonResponse({
+        "api_root": f"https://{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev/api/" if os.environ.get('CODESPACE_NAME') else "http://localhost:8000/api/"
+    })),
 ]
